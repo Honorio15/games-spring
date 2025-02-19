b0228b4e9b2ec8dc2b84b34e18894c78259bf3f8
Hilton Honorio <honoriohneto@gmail.com>
2025-02-19 17:57:39 +0000

    mensagem

--- app/src/main/java/application/model/Categoria.java
+++ app/src/main/java/application/model/Categoria.java
@@ -14,24 +14,35 @@ import jakarta.persistence.Table;
 @Entity 
 @Table(name="categorias") 
 public class Categoria { 
-@Id 
-@GeneratedValue(strategy GenerationType. IDENTITY) 
-private long id; 
-@Column(unique true, nullable = false) 
-private String nome; 
-@One ToMany (mappedBy "categoria") 
-private Set<logo jogos new HashSet<>(); 
-public long getId() {
-return id; 
-}
-public void setId(long id) ( } 
-this.id id; 
-public String gethome() ( return nome; 
-public void sethome (String nome) ( F 
-this.nome nome; 
-public Set<3ogo> getJogos() { 
-return jogos; 
-public void setJogos (Set<logos jogos) ( 
-this.jogos jogos;
-}
+    @Id 
+    @GeneratedValue(strategy = GenerationType. IDENTITY) 
+    private long Id; 
+    @Column(unique = true, nullable = false) 
+    private String nome;
+
+    @OneToMany (mappedBy = "categoria") 
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId() {
+    return id; 
+    }
+
+    public void setId(long id) { 
+        this.id = id;
+    }
+
+    public String gethome() {
+         return nome;
+    }
+
+    public void sethome (String nome) { 
+    this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos() { 
+        return jogos; 
+    }
+    public void setJogos (Set<Jogos> jogos) { 
+    this.jogos = jogos;
+    }
 }
